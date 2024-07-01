95b8c51256be673aa8cad7063604b6299a2fb991
VirtualTeki <161194320+VirtualTeki@users.noreply.github.com>
2024-07-01 23:51:39 +0700

    Create app_trackingv3.py

---
+++ app_trackingv3.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import threading
+import RPi.GPIO as GPIO
+import serial
+import sqlite3
+import datetime
+import time
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/gps', methods=['GET'])
+def get_coordinates():
+    from_date_str   = request.args.get('from',time.strftime("%Y-%m-%d %H:%M")) #Get the from date value from the URL
+    to_date_str     = request.args.get('to',time.strftime("%Y-%m-%d %H:%M"))   #Get the to date value from the URL
+    conn = sqlite3.connect('/var/www/lab_app/gps_app.db')  # change this to your database path
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM coordinates WHERE rDateTime BETWEEN ? AND ?", (from_date_str, to_date_str))
+    rows = curs.fetchall()
+    conn.close()
+
+    # Convert the data to a list of dictionaries for easy JSON conversion
+    coordinates = [{"rDatetime": row[0], "sensorID": row[1], "latitude": row[2], "longitude": row[3]} for row in rows]
+
+    return render_template('coordinates.html', coordinates=coordinates)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
